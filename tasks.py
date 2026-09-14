ca3e81084d1f5b144c185e714ba5a995e445e6d1
ZHARIKOV MAKSYM <MAKSEEEEEEMAN@MacBook-Air-Elizaveta.local>
2026-09-14 13:09:46 +0200

    Add task creation

---
+++ tasks.py
@@ -0,0 +1,12 @@
+tasks = []
+def add_task(title :str) -> None :
+    new_id = len(tasks) + 1
+    new_dict = {
+        "id": new_id,
+        "title": title,
+        "completed": False
+    }
+    tasks.append(new_dict)
+add_task("Learn Git")
+add_task("Learn Pythin Core")
+print(tasks)
