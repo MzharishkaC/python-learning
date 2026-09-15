6f16faabf9a2036f1551498753a711d215840879
ZHARIKOV MAKSYM <MAKSEEEEEEMAN@MacBook-Air-Elizaveta.local>
2026-09-15 13:57:32 +0200

    1

--- tasks.py
+++ tasks.py
@@ -9,33 +9,38 @@ def add_task(title :str) -> None :
     tasks.append(new_dict)
 add_task("Learn Git")
 add_task("Learn Pythin Core")
+
 def get_tasks() -> list:
     return tasks
-print(get_tasks())
+
 def get_completed_tasks() ->list :
     completed_task = []
     for task in tasks:
         if task["completed"] :
             completed_task.append(task)
     return completed_task
+
 def complete_task(task_id: int) -> bool:
     for task in tasks  :
         if task["id"] == task_id:
             task["completed"] = True
             return True
     return False
+
 def delete_task(task_id: int) -> bool:
     for index, task in enumerate(tasks):
         if task["id"] == task_id:
             tasks.pop(index)
             return True
     return False
+
 def update_task(task_id: int, new_title: str) ->bool:
     for task in tasks :
         if task["id"] == task_id :
             task["title"] = new_title
             return True
     return False
+
 def clear_completed_task() ->int:
     completed_task = 0
     complete_task = tasks.copy()
@@ -44,40 +49,47 @@ def clear_completed_task() ->int:
             tasks.remove(task)
             completed_task += 1
     return completed_task
+
 def get_pending_tasks() -> list:
     pending_tasks =[]
     for task in tasks:
         if task["completed"] == False:
             pending_tasks.append(task)
     return pending_tasks
+
 def get_task_by_id(task_id : int) -> dict |None:
     for task in tasks :
         if task["id"] == task_id:
             return task
     return None
+
 def count_complete_task() ->int:
     count = 0
     for task in tasks :
         if task["completed"]:
             count += 1
     return count
+
 def find_task_position(task_id: int) -> int:
     for index, task in enumerate(tasks):
         if task["id"] == task_id :
             return index
     return -1
+
 def toggle_task(task_id: int) -> bool:
     for task in tasks :
         if task["id"] == task_id:
             task["completed"] = not task["completed"]
             return True
     return False
+
 def get_incomplete_count() ->int:
     incomplete_task = 0
     for task in tasks:
         if  not task["completed"]:
             incomplete_task +=1
     return incomplete_task
+
 def get_tasks_by_status(completed: bool) -> list:
     task_status = []
     for task in tasks:
