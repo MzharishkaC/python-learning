cf50449fe5989daeb9a4b6a95b2f9fa8d9652161
ZHARIKOV MAKSYM <MAKSEEEEEEMAN@MacBook-Air-Elizaveta.local>
2026-09-16 12:12:06 +0200

    just learning merge conflict

--- tasks.py
+++ tasks.py
@@ -154,4 +154,4 @@ def change_task_status(task_id: int, status: str) -> bool:
                 task["completed"] = False
             return True
     return False
-# Working on status feature
+# Working on status feature to merge conflict
