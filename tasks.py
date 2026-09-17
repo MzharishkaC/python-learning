0a1e1e60caa770077060f729f70d1400a2def187
ZHARIKOV MAKSYM <MAKSEEEEEEMAN@MacBook-Air-Elizaveta.local>
2026-09-17 12:51:43 +0200

    addet a new function delete tasks by status

--- tasks.py
+++ tasks.py
@@ -32,14 +32,7 @@ def update_task(task_id: int, new_title: str) -> bool:
     return False
 
 
-def clear_completed_task() -> int:
-    completed_task = 0
-    complete_task = tasks.copy()
-    for task in complete_task:
-        if task["completed"]:
-            tasks.remove(task)
-            completed_task += 1
-    return completed_task
+
 
 
 def get_task_by_id(task_id: int) -> dict | None:
@@ -49,15 +42,11 @@ def get_task_by_id(task_id: int) -> dict | None:
     return None
 
 
-
-
 def find_task_position(task_id: int) -> int:
     for index, task in enumerate(tasks):
         if task["id"] == task_id:
             return index
     return -1
-
-
 
 
 def find_task(keyword: str) -> list:
@@ -69,15 +58,6 @@ def find_task(keyword: str) -> list:
 
 
 
-def sort_tasks_by_status() -> list:
-    sorted_tasks = []
-    for task in tasks:
-        if task["completed"] == False:
-            sorted_tasks.append(task)
-    for task in tasks:
-        if task["completed"]:
-            sorted_tasks.append(task)
-    return sorted_tasks
 
 
 def get_tasks_by_status(status: str) -> list:
@@ -95,6 +75,7 @@ def change_task_status(task_id: int, status: str) -> bool:
             return True
     return False
 
+
 def get_tasks_amount_by_status(status: str) -> int:
     count = 0
     for task in tasks:
@@ -102,13 +83,22 @@ def get_tasks_amount_by_status(status: str) -> int:
             count += 1
     return count
 
+
 def get_tasks_stats() -> dict:
     total = len(tasks)
-    completed =  get_tasks_amount_by_status("completed")
-    pending =  get_tasks_amount_by_status("pending")
+    completed = get_tasks_amount_by_status("completed")
+    pending = get_tasks_amount_by_status("pending")
 
     return {
         "total": total,
         "completed": completed,
         "pending": pending
     }
+def delete_tasks_by_status(status: str) -> int:
+    count = 0
+    deleted_tasks = tasks.copy()
+    for task in deleted_tasks:
+        if task["status"] == status:
+            tasks.remove(task)
+            count += 1
+    return count
